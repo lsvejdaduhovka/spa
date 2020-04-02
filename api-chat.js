89eb5e61253bc4ad4ea087f38d4164b1de62b381
Libor Svejda <libor.svejda@gmail.com>
2020-04-02 08:35:51 +0200

    text zpravy z parametru formatovani casu zpravy

--- api-chat.js
+++ api-chat.js
@@ -6,6 +6,9 @@ exports.apiChat = function (req, res, obj) {
     if (req.pathname.endsWith("/list")) {
         obj.messages = msgs;
     } else if (req.pathname.endsWith("/add")) {
-        msgs.push("test");
+        let obj = {};
+        obj.message = req.parameters.msg; //text zpravy z parametru msg
+        obj.time = dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss");
+        msgs.push(obj);
     }
 }
